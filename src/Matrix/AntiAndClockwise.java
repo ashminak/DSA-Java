63676297d130cc6f0abcc1f2036a36a474195ec1
ashminak <122225680+ashminak@users.noreply.github.com>
2026-04-17 12:34:12 +0530

    Implement AntiAndClockwise.java in Matrix
    Implement AntiAndClockwise.java in Matrix

---
+++ src/Matrix/AntiAndClockwise.java
@@ -0,0 +1,68 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class AntiAndClockwise {
+    public void clockwise(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int left =0;int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+    }
+    public void antiClockwise(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0;int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns: ");
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        System.out.println("Enter the elements of the matrix: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        AntiAndClockwise anti = new AntiAndClockwise();
+        anti.clockwise(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+
+    }
+}
