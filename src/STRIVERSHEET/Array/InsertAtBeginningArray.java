45654286820516c7650993c2e64f96957253a7df
ashminak <122225680+ashminak@users.noreply.github.com>
2026-05-20 19:55:01 +0530

    Implement InsertAtBeginningArray.java in Array in STRIVERSHEET
    Implement InsertAtBeginningArray.java in Array in STRIVERSHEET

---
+++ src/STRIVERSHEET/Array/InsertAtBeginningArray.java
@@ -0,0 +1,31 @@
+package STRIVERSHEET.Array;
+
+import java.util.Scanner;
+
+public class InsertAtBeginningArray {
+    public int[] Solution(int[] arr, int x){
+        int[] ans = new int[arr.length+1];
+        ans[0]=x;
+        for(int i=0;i<arr.length;i++){
+            ans[i+1]=arr[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+       int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element to insert at beginning: ");
+        int x = sc.nextInt();
+        InsertAtBeginningArray obj = new InsertAtBeginningArray();
+        int[] tem= obj.Solution(arr,x);
+        for (int i : tem) {
+            System.out.print(i+" ");
+        }
+    }
+}
