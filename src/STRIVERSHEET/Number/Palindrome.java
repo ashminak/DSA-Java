94e199d73e86d3c310cd884531e5a4f50734d2b0
ashminak <122225680+ashminak@users.noreply.github.com>
2026-05-23 19:42:56 +0530

    Update

--- src/STRIVERSHEET/Number/Palindrome.java
+++ src/STRIVERSHEET/Number/Palindrome.java
@@ -3,19 +3,20 @@ package STRIVERSHEET.Number;
 import java.util.Scanner;
 
 public class Palindrome {
-    public boolean Solution(int n){
-        int revNum =0;
+    public boolean Solution(int n) {
+        int revNum = 0;
         int dup = n;
-        while (n>0){
-            int id = n%10;
-            revNum = (revNum*10) + id;
-            n=n/10;
+        while (n > 0) {
+            int id = n % 10;
+            revNum = (revNum * 10) + id;
+            n = n / 10;
         }
-        if(revNum==dup){
+        if (revNum == dup) {
             return true;
         }
         return false;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number:");
@@ -24,4 +25,5 @@ public class Palindrome {
         boolean result = p.Solution(n);
         System.out.println(result);
     }
+
 }
