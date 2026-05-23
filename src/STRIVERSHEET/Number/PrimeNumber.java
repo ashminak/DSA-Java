b332f7547266aa2e4744948c1b7e960479a8a61a
ashminak <122225680+ashminak@users.noreply.github.com>
2026-05-23 19:40:39 +0530

    Implement PrimeNumber.java in Number in STRIVERSHEET
    Implement PrimeNumber.java in Number in STRIVERSHEET

--- src/STRIVERSHEET/Number/PrimeNumber.java
+++ src/STRIVERSHEET/Number/PrimeNumber.java
@@ -2,25 +2,49 @@ package STRIVERSHEET.Number;
 
 import java.util.Scanner;
 
-public class PrimeNumber {
-    public boolean Solution(int n){
-        int count =0;
-        for(int i =1;i<=n;i++){
-            if(n%i==0){
+//public class PrimeNumber {
+//    public boolean Solution(int n){
+//        int count =0;
+//        for(int i =1;i<=n;i++){
+//            if(n%i==0){
+//
+//            }
+//        }
+//        if (count==2){
+//            return true;
+//        }
+//        return false;
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number");
+//        int n = sc.nextInt();
+//        PrimeNumber p = new PrimeNumber();
+//        boolean result = p.Solution(n);
+//        System.out.println(result);
+//    }
+//}
 
+public class PrimeNumber {
+    public boolean isPrime(int n){
+        int count =0;
+        for (int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                count++;
+                if(n/i!=i){
+                    count++;
+                }
             }
+
         }
-        if (count==2){
-            return true;
-        }
-        return false;
+        return count==2;
     }
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
         PrimeNumber p = new PrimeNumber();
-        boolean result = p.Solution(n);
+        boolean result = p.isPrime(n);
         System.out.println(result);
     }
 }
