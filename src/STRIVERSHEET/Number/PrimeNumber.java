3301dc0ec2fada4f858a5cc605247e971c3ea527
ashminak <122225680+ashminak@users.noreply.github.com>
2026-05-23 14:17:14 +0530

    Implement PrimeNumber.java in Number in STRIVERSHEET
    Implement PrimeNumber.java in Number in STRIVERSHEET

---
+++ src/STRIVERSHEET/Number/PrimeNumber.java
@@ -0,0 +1,26 @@
+package STRIVERSHEET.Number;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public boolean Solution(int n){
+        int count =0;
+        for(int i =1;i<=n;i++){
+            if(n%i==0){
+
+            }
+        }
+        if (count==2){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        PrimeNumber p = new PrimeNumber();
+        boolean result = p.Solution(n);
+        System.out.println(result);
+    }
+}
