ee8adeae885666c5363af72dbb324b53d135a330
ashminak <122225680+ashminak@users.noreply.github.com>
2026-06-03 03:39:24 +0530

    Implement insertionSorting.java in Sorting in STRIVERSHEET
    Implement insertionSorting.java in Sorting in STRIVERSHEET

---
+++ src/STRIVERSHEET/Sorting/insertionSorting.java
@@ -0,0 +1,33 @@
+package STRIVERSHEET.Sorting;
+
+import java.util.Scanner;
+
+public class insertionSorting {
+    public void  insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i=1;i<n-1;i++){
+            int key = arr[i];
+            int j=i-1;
+            while(j>=0&&arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in the array: ");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        insertionSorting obj = new insertionSorting();
+        obj.insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
